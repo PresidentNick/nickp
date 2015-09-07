5477a0c31c4ad3f627faa1f4fb2f39dd8f15e256
Nick <nickpereira.np@gmail.com>
2015-09-07 11:24:22 -0500

    removed email info

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ from datetime import datetime
 YOUR_INFO = {
     'name' : 'Nick Pereira',
     'bio' : "I'm a junior developer for Interline Vacations and a senior studying CS at UT",
-    'email' : 'nickpereira.np@gmail.com', # Leave blank if you'd prefer not to share your email with other conference attendees
+    'email' : '', # Leave blank if you'd prefer not to share your email with other conference attendees
     # 'twitter_username' : '', # No @ symbol, just the handle.
     'github_username' : "PresidentNick", 
     'headshot_url' : 'https://avatars1.githubusercontent.com/u/8160053?v=3&s=460', # Link to your GitHub, Twitter, or Gravatar profile image.
